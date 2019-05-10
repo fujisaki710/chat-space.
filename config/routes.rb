223f4d019d1cb2240fb2eb4ae9e0fc8f52c5a350
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-05-10 16:05:18 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]  
   end
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
