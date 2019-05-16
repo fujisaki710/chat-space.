4b2865ee5112cc333c3608753eb470b62d733649
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-05-16 16:52:46 +0900

    テキストフィールド入力

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,24 @@
+$(function() {
+    $(".chat-group-form").on("keyup", function() {
+      var input = $(".chat-group-form").val();
+
+      $.ajax({
+        type: 'GET',
+        url: '/users/index',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+      
+      .done(function(users) {
+        $(".chat-group-form__field.clearfix").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendProduct(user);
+          });
+        }
+        else {
+          appendErrMsgToHTML("一致するユーザーはいません");
+        }
+      })      
+    });
+});
