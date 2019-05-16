34b47db0b56707fadb4c09ccd61b1b5dcae4977a
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-05-16 20:36:22 +0900

    インクリメンタルサーチ２

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,11 +4,11 @@ $(function(){
 
   function appendUsers(user) {
     var html =`<div class="chat-group-user clearfix js-chat-user">
-                <p class="chat-group-user__name">
-                ${user.name}
-                </p>
-                <a class="user_search_add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
-                </a>
+                  <p class="chat-group-user__name">
+                    ${user.name}
+                  </p>
+                  <a class="user_search_add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
+                  </a>
                 </div>`
 
     user_list.append(html);
