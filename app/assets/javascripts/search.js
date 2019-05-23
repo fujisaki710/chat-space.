31502be7269d20391726e6c33230ab0e328628b2
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-05-23 12:35:55 +0900

    a

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -13,8 +13,7 @@ $(function(){
     }
   
     $("#user-search-field").on("keyup", function() {
-        var input = $("#user-search-result").val();
-  
+        var input = $("#user-search-field").val();
         $.ajax({
           url: '/users',
           type: "GET",
@@ -42,10 +41,7 @@ $(function(){
       var html = `<div class='chat-group-user clearfix' id='chat-group-user-${user_id}'>
             <input name='group[user_ids][]' type='hidden' value= ${user_id}>
               <p class='chat-group-user__name'>${user_name}</p>
-                <a class='user-search-remove
-                 chat-group-user__btn
-                 chat-group-user__btn--remove
-                 js-remove-btn'>削除</a>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
           </div>`
       return html;
     }
@@ -55,7 +51,7 @@ $(function(){
       var user_id = $(this).attr("data-user-id");
       var user_name = $(this).attr("data-user-name");
       var html = appendUser(user_id, user_name);
-      $('#add-user-list').append(html)
+      $('#chat-group-users').append(html)
       $(this).parent().remove();
     })
   })
