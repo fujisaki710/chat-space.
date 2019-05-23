bd46388fc3cf7683409020ee45c38e49f1d87106
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-05-23 17:46:20 +0900

    b

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $ (function() {
   function buildHTML(message){
     var imageHTML = message.image.url ? `<img src="${message.image.url}" class="lower-message__image" >` : "";
     var html =
-        `<div class="message" data-id=${message.id}>
+        `<div class="message" data-message_id=${message.id}>
             <div class="upper-message">
               <div class="upper-message__user-name">
                 ${message.user_name}
@@ -20,7 +20,7 @@ $ (function() {
             ${imageHTML} 
           </div>`
         return html;
-  }
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
